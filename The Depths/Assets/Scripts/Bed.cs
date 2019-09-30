c2b882e0eb4e1d6cc699d481b2bcfa65b33a486e
Wyatt Joyner <wyatt.joyner@practicalcode.us>
2019-09-30 10:46:39 -0400

    bed(rock) added eksdee

---
+++ The Depths/Assets/Scripts/Bed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bed : Interactable
+{
+    Transform bed_point;
+
+    private void Start()
+    {
+        bed_point = transform.Find("bed_point");
+        if (!bed_point) bed_point = Instantiate(new GameObject(), transform).transform;
+        bed_point.name = "bed_point";
+    }
+
+    public override void Interact()
+    {
+        //GameManager.instance.player.Sleep(bed_point);
+        print("I'm a bed, haha!");
+    }
+}
