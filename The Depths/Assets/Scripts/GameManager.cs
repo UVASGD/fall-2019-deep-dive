94ad6a1de90f9e666289b80142ab9c7be1ca9236
AidanHimley <55163434+AidanHimley@users.noreply.github.com>
2019-11-01 21:36:48 -0400

    Added bar UIs for health and xp
    Everything works when launching from any scene, but xp is broken when going between scenes for some reasons. Monsters give xp on death

---
+++ The Depths/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using Gamekit2D;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public LevelXP xp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xp = GetComponentInChildren<LevelXP>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
