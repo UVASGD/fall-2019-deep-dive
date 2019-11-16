b62c5a6bdf5575e848a173a3c0042d2662a0cfd9
jimmyc5 <55065561+jimmyc5@users.noreply.github.com>
2019-11-15 21:49:40 -0500

    Death-Reset-to-Hub

--- The Depths/Assets/2DGamekit/Scripts/Character/StateMachineBehaviours/Player/DeadSMB.cs
+++ The Depths/Assets/2DGamekit/Scripts/Character/StateMachineBehaviours/Player/DeadSMB.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-
+using UnityEngine.SceneManagement;
 namespace Gamekit2D
 {
     public class DeadSMB : SceneLinkedSMB<PlayerCharacter>
@@ -7,12 +7,14 @@ namespace Gamekit2D
         public override void OnSLStatePostEnter (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             m_MonoBehaviour.SetMoveVector(m_MonoBehaviour.GetHurtDirection() * m_MonoBehaviour.hurtJumpSpeed);
+            SceneManager.LoadScene(1);
         }
 
         public override void OnSLStateNoTransitionUpdate (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
             m_MonoBehaviour.CheckForGrounded();
             m_MonoBehaviour.AirborneVerticalMovement();
+
         }
 
         public override void OnSLStateExit (Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
